fed9f9f7f68f29020dd76bcc541b9fa9cc34b9ff
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-10-28 02:18:22 -0700

    added working source

--- app.py
+++ app.py
@@ -35,15 +35,14 @@ def new():
             flash('Please enter all the fields;', 'error')
         else:
             student = students(request.form['name'], request.form['city'], 
-                  request.form['addr'], request.form['pin'])
+               request.form['addr'], request.form['pin'])
 
             db.session.add(student)
             db.session.commit()
             flash('Record was successfully added')
             return redirect(url_for('show_all'))
-        return render_template('new.html')
+    return render_template('new.html')
 
-        
 if __name__ == '__main__':
     db.create_all()
     app.run(debug = True)
